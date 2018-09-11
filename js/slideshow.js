48e677d5d2361b4b3923670130f92a84a59ecfd6
tr-vx <trevx10@gmail.com>
2018-09-11 11:14:37 +0200

    Ajout du fichier slideshow.js / liens fichier html

---
+++ js/slideshow.js
@@ -0,0 +1,26 @@
+// Création de l'objet
+var slideshow = {
+
+	// Nombre de slide total
+	slides : 7,
+	currentSlide: 1,
+
+	// Initialisation
+	init: function () {
+
+	},
+
+	// Revenir au slide précedent
+	previous: function () {
+
+	},
+
+	// Passer au slide suivant
+	next: function () {
+
+	}
+
+
+};
+
+slideshow.init();
